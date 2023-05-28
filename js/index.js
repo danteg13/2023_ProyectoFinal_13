aa315b0537f8746f9d4949248f89d1ae78ef9bf2
Dante Gennari <soydante@yahoo.com>
2023-05-29 01:39:15 +0200

    insercion de filtro de generos

--- js/index.js
+++ js/index.js
@@ -24,6 +24,35 @@ const playArrows = document.querySelectorAll('.fa-play-circle');
 
 // Iterar sobre cada flecha de reproducción y cambiar el color a verde
 playArrows.forEach((arrow) => {
-  arrow.style.color = '#00ff00'; // Cambia el color a verde (#00ff00)
+  arrow.style.color = '#00ff00'; // Cambia el color de la flecha a verde (#00ff00)
 });
 
+// Filtrar generos
+window.addEventListener('DOMContentLoaded', function() {
+  const genreButtons = document.querySelectorAll('.genre-button');
+  const genreContainer = document.querySelector('.genre-container');
+
+  genreButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      const genre = button.getAttribute('data-genre');
+
+      // Mostrar todos los videos
+      if (genre === 'todos') {
+        genreContainer.querySelectorAll('.video2').forEach(function(video) {
+          video.style.display = 'block';
+        });
+      } else {
+        // Ocultar los videos que no coinciden con el género seleccionado
+        genreContainer.querySelectorAll('.video2').forEach(function(video) {
+          const videoGenre = video.getAttribute('data-genre');
+
+          if (videoGenre === genre) {
+            video.style.display = 'block';
+          } else {
+            video.style.display = 'none';
+          }
+        });
+      }
+    });
+  });
+});
