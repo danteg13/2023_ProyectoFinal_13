190cb4bee01eb37fd78367f0457ac03e822bb2fe
Dante Gennari <soydante@yahoo.com>
2023-06-04 03:49:59 +0200

    sigo intentando implementar firebase firestore al proyecto

--- js/form.js
+++ js/form.js
@@ -1,22 +1,3 @@
-// Importar las funciones necesarias de Firebase
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
-// Tu configuración de Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyAn5ft3WQReuUxd8-TYOCxhzGnEc11IFSw",
-  authDomain: "trailerbrasil-ff859.firebaseapp.com",
-  projectId: "trailerbrasil-ff859",
-  storageBucket: "trailerbrasil-ff859.appspot.com",
-  messagingSenderId: "41682364242",
-  appId: "1:41682364242:web:434555c8df0a82b4febc8b",
-  measurementId: "G-7G5WEN38LT"
-};
-
-// Inicializar Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
 // Obtener referencia al formulario de registro
 const registerForm = document.getElementById("registerForm");
 
@@ -27,12 +8,13 @@ registerForm.addEventListener("submit", function(event) {
   // Obtener los valores de los campos del formulario
   const username = document.getElementById("username").value;
   const nombre = document.getElementById("nombre").value;
+  const fecha = document.getElementById("fecha").value;
   const correo = document.getElementById("correo").value;
   const password = document.getElementById("password").value;
   const confirmPassword = document.getElementById("confirmPassword").value;
 
   // Validar que todos los campos estén completos
-  if (username === '' || nombre === '' || correo === '' || password === '' || confirmPassword === '') {
+  if (username === '' || nombre === '' || fecha === '' || correo === '' || password === '' || confirmPassword === '') {
     alert("Por favor, complete todos los campos.");
     return;
   }
@@ -43,17 +25,32 @@ registerForm.addEventListener("submit", function(event) {
     return;
   }
 
-  // Registrar al usuario en Firebase
-  register(correo, password);
+  // Registrar al usuario en Firebase Authentication
+  register(correo, password)
+    .then(() => {
+      // Agregar datos del usuario a Firestore
+      addUserToFirestore(username, nombre, fecha, correo)
+        .then(() => {
+          console.log("Usuario registrado y datos guardados en Firestore");
+          alert("¡Registro exitoso! Ahora puedes iniciar sesión.");
+        })
+        .catch(error => {
+          console.error("Error al guardar los datos del usuario en Firestore:", error);
+        });
+    })
+    .catch(error => {
+      console.error("Error registrando al usuario en Firebase Authentication:", error);
+    });
 });
 
-// Función para registrar al usuario en Firebase
-async function register(email, password) {
-  try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    console.log("Usuario registrado: ", userCredential.user);
-    alert("¡Registro exitoso! Ahora puedes iniciar sesión.");
-  } catch (e) {
-    console.error("Error registrando al usuario: ", e);
-  }
+// Función para registrar al usuario en Firebase Authentication
+function register(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+// Función para agregar datos del usuario a Firestore
+function addUserToFirestore(username, nombre, fecha, correo) {
+  const userCollection = collection(db, "users");
+  const userData = { username, nombre, fecha, correo };
+  return addDoc(userCollection, userData);
 }
